20aac8915c1a8d5529e361e7e4c7eceac02e581f
Tomáš Svoboda <Stomik04@gmail.com>
2025-08-28 08:58:31 +0200

    3446. Sort Matrix by Diagonals

---
+++ daily questions/3446. Sort Matrix by Diagonals/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Solution
+{
+    public int[][] SortMatrix(int[][] grid)
+    {
+        int n = grid.Length;
+        for (int i = 0; i < n; i++)
+        {
+
+
+        }
+
+    }
+}
+public class Program
+{
+    public void Main(string[] args)
+    {
+        Solution s = new Solution();
+        int[][] grid = [[1, 7, 3], [9, 8, 2], [4, 5, 6]];
+        int[][] result = s.SortMatrix(grid);
+        Console.WriteLine(result);
+        
+    }
+        
+}
+
